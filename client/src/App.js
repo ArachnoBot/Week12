e853102231b6a8acdf6ba05145c848ed9ca1ff0c
Leevi Lautanen <leevi.lautanen02@gmail.com>
2024-01-13 16:55:09 +0200

    changed form to fetch

--- client/src/App.js
+++ client/src/App.js
@@ -10,21 +10,39 @@ function App() {
     })
   }
 
+  const addBook = () => {
+    const name = document.getElementById("name")
+    const author = document.getElementById("author")
+    const pages = document.getElementById("pages")
+
+    fetch("/api/book", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: name.value,
+        author: author.value,
+        pages: pages.value
+      })
+    })
+  }
+
   return (
     <div className="App">
       <h1>Books</h1>
-      <form method='POST' action='/api/book'>
-        <label>Title: </label>
-        <input type='text' id="name" name="name"></input>
-        <br></br>
-        <label>Author: </label>
-        <input type='text' id="author" name="author"></input>
-        <br></br>
-        <label>Number of pages: </label>
-        <input type='number' id="pages" name="pages"></input>
-        <br></br>
-        <input type='submit' id="submit" name="submit"></input>
-      </form>
+
+      <label>Title: </label>
+      <input type='text' id="name"></input>
+      <br></br>
+      <label>Author: </label>
+      <input type='text' id="author"></input>
+      <br></br>
+      <label>Number of pages: </label>
+      <input type='number' id="pages"></input>
+      <br></br>
+      <input type='submit' id="submit" onClick={addBook}></input>
+
       <input type="button" value="get books" onClick={fetchBooks}></input>
     </div>
   );

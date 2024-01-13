30d1963ea3b563a5ba5105c57a5e2bc404e2c2ce
Leevi Lautanen <leevi.lautanen02@gmail.com>
2024-01-13 16:31:45 +0200

    removed api book

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,8 +14,7 @@ const BookSchema = mongoose.Schema({
 
 const Books = mongoose.model("Books", BookSchema)
 
-
-router.post('/api/book', function(req, res, next) {
+router.post('/api/bookk', function(req, res, next) {
   Books.create({
     name: req.body.name,
     author: req.body.author,
